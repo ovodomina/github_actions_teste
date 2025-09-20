bfddd2d1b0f576d4dd3891205624e50a8cfb3f1e
ovodomina <139710321+ovodomina@users.noreply.github.com>
2025-09-20 11:58:10 -0300

    update_teste
    corrigido teste unitário

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -3,7 +3,7 @@ import pytest
 from src.calculadora import somar, dividir
 
 def test_somar():
-    assert somar(2, 3) == 6
+    assert somar(2, 3) == 5
     assert somar(-1, 1) == 0
     
 def test_dividir():
